a8c0b360aa4c36df760246d0e29de8d39c7c86dc
salazarm <salazarm@users.noreply.github.com>
2022-01-18 09:55:24 -0500

    Update Assist.js

--- scraper/src/discord/Assist.js
+++ scraper/src/discord/Assist.js
@@ -1,13 +1,13 @@
 import { logMessage } from './logger';
 
 export async function genRequestAssist(options) {
-  if (window.d13cfsua) {
-    const assist = await window.d13cfsua(options);
+  if (window.clientQueueAction) {
+    const assist = await window.clientQueueAction(options);
     return assist;
   }
   return await new Promise(res => {
-    window.d13cfsua = async function(fn) {
-      delete window.d13cfsua;
+    window.clientQueueAction = async function(fn) {
+      delete window.clientQueueAction;
       const result = await fn(options);
       res(result);
     };
@@ -18,15 +18,15 @@ export async function genServerReceiveAssist(receiveCommand) {
   if (window.d13cfsua) {
     window.d13cfsua(assist)
   }
-  window.d13cfsua = function(options) {
-    delete window.d13cfsua;
+  window.clientQueueAction = function(options) {
+    delete window.clientQueueAction;
     assist(options);
   }
 
    async function assist(options) {
     const promise = new Promise((res) => {
-      window.d14cfsua = function(info) {
-        delete window.d14cfsua;
+      window.serverResponse = function(info) {
+        delete window.serverResponse;
         res(info);
       }
     });
@@ -38,8 +38,7 @@ export async function genServerReceiveAssist(receiveCommand) {
 
 export async function genServerAssistResponse(info, res) {
   if (window.d14cfsua) {
-    window.d14cfsua(info);
-    delete window.d14cfsua;
+    window.serverResponse(info);
     res({success: true});
   } else {
     res({success: false});
